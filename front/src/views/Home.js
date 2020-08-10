2ebcb14cbb3d30b7f82b543bec7be0c036c2e20e
Gustavo Zuppa <gustavo.zuppa@ulfix.com>
2020-08-10 01:48:27 -0500

    create Grid and gql query

--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { Grid } from 'semantic-ui-react'
+import MissionCard from '../components/MissionCard'
 
 function Home() {
+    const { loading, data: { getMissions: missions } } = useQuery(FETCH_MISSIONS)
+  
     return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <Grid columns={3}>
+        <Grid.Row>
+            <h1>Recent Missions</h1>
+        </Grid.Row>
+        <Grid.Row>
+          {loading ? (
+              <h1>Missions on loading...</h1> 
+          ) : (
+              missions && missions.map(mission => (
+                  <Grid.Column key={mission.id}>
+                      <MissionCard mission={mission} />
+                  </Grid.Column>
+              ))
+          )}
+        </Grid.Row>
+        </Grid>
     )
 }
+
+const FETCH_MISSIONS = gql`
+    {
+        getMissions{
+        missionName 
+        resultSuccess
+        id
+        image
+        date
+        wiki
+        about
+        }
+    }
+`
 
 export default Home
