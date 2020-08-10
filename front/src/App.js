abb09e3d7d1a3386896f759266d691d09af5ac11
Gustavo Zuppa <gustavo.zuppa@ulfix.com>
2020-08-10 00:31:12 -0500

    navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+
+import NavBar from './components/NavBar'
+import Home from './views/Home'
+import Login from './views/Login'
+import Register from './views/Register'
 
 function App() {
   return (
-    <div>
-      <h1>
-        Hola
-      </h1>
-    </div>
+    <Router>
+      <Container>
+      <NavBar/>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
   );
 }
 
